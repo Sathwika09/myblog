a35681067e63e9430f58603576777f386ac68a45
Sathwika09 <ssathwika855@gmail.com>
2024-04-06 17:34:29 +0530

    changes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-export const signup=async (req,res) => {
+import { errorHandler } from '../utils/error.js';
+export const signup=async (req,res,next) => {
     const {username,email,password}=req.body;
 
     if (
@@ -25,7 +26,7 @@ export const signup=async (req,res) => {
         await newUser.save();
         res.json('Signup successful');
     } catch (error) {
-        res.status(500).json({message: error.message});
+        next(error);
     }
        
     
