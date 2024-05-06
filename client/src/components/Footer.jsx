49a27bf77047dd102f7ddc47875cbbde0c402857
Sathwika09 <ssathwika855@gmail.com>
2024-05-06 11:17:23 +0530

    ui changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,22 +12,16 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Sathwika's
+                Sathwika's Blog
               </span>
-              Blog
+              
             </Link>
           </div>
           <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
             <div>
               <Footer.Title title='About' />
               <Footer.LinkGroup col>
-                <Footer.Link
-                  href='https://www.100jsprojects.com'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  100 JS Projects
-                </Footer.Link>
+                
                 <Footer.Link
                   href='/about'
                   target='_blank'
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React from "react";
+import me from "C:/Users/dell/Desktop/myblog/me.jpeg"
 export default function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex items-center justify-center'>
+  <div className='max-w-2xl mx-auto p-3 text-center'>
+    <div className='flex items-center justify-center mb-5'>
+      <img
+        src={me}
+        alt='Sathwika Ganneboina'
+        className='w-30 h-40 rounded-full mr-3'
+      />
+      <h1 className='text-3xl font-semibold text-center'>About Sathwika's Blog</h1>
     </div>
-  )
+    <div className='text-md text-gray-500 flex flex-col gap-6'>
+      <p>
+        Welcome to Sathwika's Blog! This blog was created by Sathwika Ganneboina
+        as a personal project to share my thoughts and ideas with the
+        world. I am a passionate developer who loves to write about general stuff
+        technology, coding, and everything in between.
+      </p>
+
+      <p>
+        On this blog, you'll find weekly articles and tutorials on topics
+        such as web development,current day scenarios , software engineering, and programming
+        languages. is always learning and exploring new
+        technologies, so be sure to check back often for new content!
+      </p>
+
+      <p>
+        Iam glad to see my followers leave comments on my posts and engage with
+        other readers. You can like other people's comments and reply to
+        them as well. We believe that a community of learners can help
+        each other grow and improve.
+      </p>
+    </div>
+  </div>
+</div>
+ );
 }
